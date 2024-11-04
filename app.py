d9a5d356c70d6f2df258625d49dd5e1fcefc1700
Antony Thornton <131768304+Antony-Thornton-Amtivo@users.noreply.github.com>
2024-11-04 16:04:09 +0000

    Tried adding code to fix CSP errors but no luck. Putting this on the shelf until i hear from DT.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,make_response
 import os
 
 if os.path.exists("env.py"):
@@ -16,6 +16,18 @@ app.debug = True
 @app.route("/")
 def home():
     return render_template("base.html")
+
+@app.after_request
+def add_csp_header(response):
+    csp = (
+        "default-src 'self'; "
+        "style-src 'self' 'unsafe-inline'; "
+        "frame-src 'self' https://bab.eu.qlikcloud.com https://login.qlik.com; "
+        "connect-src 'self' https://bab.eu.qlikcloud.com; "
+        "frame-ancestors 'self' https://myqlik.qlik.com;"
+    )
+    response.headers['Content-Security-Policy'] = csp
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)  # No debug=True for production
