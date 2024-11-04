907307e5c0280e1c9257ff9bdc11ab603430647e
Antony Thornton <131768304+Antony-Thornton-Amtivo@users.noreply.github.com>
2024-11-04 12:05:40 +0000

    Render 502 error debugging. Adding code to app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+import os
+
+if os.path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
+
+os.environ['FLASK_ENV'] = 'development'
+
+app.debug = True
+
 
 # Define the home route
 @app.route("/")
@@ -8,4 +18,4 @@ def home():
     return render_template("base.html")
 
 if __name__ == "__main__":
-    app.run()  # No debug=True for production
+    app.run(host='0.0.0.0', port=5000)  # No debug=True for production
